3f5a4b179125c6172e6f64e2c56fb5711cce7817
Ash Maher <ashkanmaherbus@gmail.com>
2017-10-05 18:15:28 -0700

    Final Initialization

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
-var express = require('express');
-var method- override = require('method-override');
-var body-parser = require('body-parser');
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require('method-override');
+var exphbs = require("express-handlebars");
+var db = require("./models");
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+// app.use(express.static("public"));
+app.use(methodOverride("_method"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+require("./routes/api-routes.js")(app);
+db.sequelize.sync();
+
+app.listen(port, function() {
+  console.log("App listening on PORT " + port);
+});
